24e09d03b91751a6bf285226c37508c2f3403234
ashwini013 <69732785+ashwini013@users.noreply.github.com>
2024-10-24 20:08:34 +0530

    hierachical inheritance.py

---
+++ hierachical inheritance.py
@@ -0,0 +1,15 @@
+class parent():
+    def pdisplay(self):
+        print("parent")
+class child1(parent):
+    def c1display(self):
+        print("child1")
+class child2(parent):
+    def c2display(parent):
+        print("child2")
+c1=child1()
+c2=child2()
+c1.pdisplay()
+c1.c1display()
+c2.pdisplay()
+c2.c2display()
